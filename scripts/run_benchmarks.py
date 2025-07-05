596bc073ce2bcaabe59c2163f5d661e29f411e60
blueloveTH <blueloveth@foxmail.com>
2025-07-05 13:53:59 +0800

    minor fix

--- scripts/run_benchmarks.py
+++ scripts/run_benchmarks.py
@@ -76,6 +76,6 @@ if __name__ == "__main__":
     # tag as index
     df = pd.DataFrame(data)
     df.set_index('tag', inplace=True, drop=True)
-    df.to_csv('output/benchmark_results.csv', index=True)
+    df.to_csv('output/results.csv', index=True)
 
     print("ALL TESTS PASSED")
